2ef017d674782495250f2a7220d14a7dbcd42950
wamunyima3 <wamunyimamukelabai3@gmail.com>
2024-10-29 10:41:27 +0200

    Changed the app name, fixed the gitub icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "YOOM",
+  title: "LACHAT",
   description: "Video calling App",
   icons: {
     icon: "/icons/logo.svg",
@@ -27,7 +27,7 @@ export default function RootLayout({
         appearance={{
           layout: {
             socialButtonsVariant: "iconButton",
-            logoImageUrl: "/icons/yoom-logo.svg",
+            logoImageUrl: "/icons/lachat-logo.svg",
           },
           variables: {
             colorText: "#fff",
